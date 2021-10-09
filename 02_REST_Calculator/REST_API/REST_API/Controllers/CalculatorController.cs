8852ddcc9d00805f75a6fac655063d9af6a5b5d9
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-08 22:32:37 -0300

    implementing calculator

---
+++ 02_REST_Calculator/REST_API/REST_API/Controllers/CalculatorController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+       
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        [HttpGet("multiply/{firstNumber}/{secondNumber}")]
+        [HttpGet("divide/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber,string secondNumber)
+        {
+            string path = HttpContext.Request.Path;
+            Console.WriteLine(path);
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            //caso funcione a funcao ira retornar uma soma, caso contrario ira retornar um bad request
+            
+            return BadRequest("Invalid Input");
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            // Checa se a strNumber é numerico, se for retorna true
+            bool isNumber = double.TryParse(strNumber, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out number); 
+            return isNumber;
+        }
+    }
+}
