55e7b725f0ae053b524a48aeb1bfdf25aa7de850
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-20 19:02:13 -0300

    Generic repositories

---
+++ 08_REST_Person_Generic_Repository/REST_API/REST_API/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using REST_API.Model.Base;
+using System;
+using System.Collections.Generic;
+
+namespace REST_API.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        public T Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
