dd2baa949ab5dff266cdd6e12e81a24798d96ac2
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-27 14:14:20 -0300

    Authentication

---
+++ 16_REST_PATCH/REST_API/REST_API/Business/IPersonBusiness.cs
@@ -0,0 +1,15 @@
+﻿using REST_API.Data.VO;
+using System.Collections.Generic;
+
+namespace REST_API.Business
+{
+    public interface IPersonBusiness
+
+    {
+        PersonVO Create(PersonVO person);
+        PersonVO FindById(long id);
+        List<PersonVO> FindAll();
+        PersonVO Update(PersonVO person);
+        void Delete(long id);
+    }
+}
---
+++ 16_REST_PATCH/REST_API/REST_API/Data/VO/BookVO.cs
@@ -0,0 +1,19 @@
+﻿using REST_API.Hypermedia;
+using REST_API.Hypermedia.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_API.Data.VO
+{
+    public class BookVO : ISupportsHyperMedia
+    {
+        public long Id { get; set; }
+        public string Author { get; set; }       
+        public DateTime Launch_Date { get; set; }       
+        public decimal Price { get; set; }      
+        public string Title { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
