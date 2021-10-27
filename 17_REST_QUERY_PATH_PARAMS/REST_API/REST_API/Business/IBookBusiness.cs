bfaea6614397d26c9cb0c7e6d2093c967ca3a5ce
Vitor Barreto <vittor_barreto@hotmail.com>
2021-10-27 15:23:48 -0300

    PATCH verb implementation

---
+++ 17_REST_QUERY_PATH_PARAMS/REST_API/REST_API/Business/IBookBusiness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using REST_API.Model.Context;
+using REST_API.Model;
+using REST_API.Data.VO;
+
+namespace REST_API.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO book);
+        BookVO FindById(long id);
+        List<BookVO> FindAll();
+        BookVO Update(BookVO book);
+        void Delete(long id);
+
+    }
+}
