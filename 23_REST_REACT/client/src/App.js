68717ada839a8913366753360b914ce716b2b9ac
Vitor Barreto <vittor_barreto@hotmail.com>
2021-11-05 23:16:49 -0300

    states with reactjs

--- 23_REST_REACT/client/src/App.js
+++ 23_REST_REACT/client/src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './Header'
 
+
 export default function App() {
+  // soh eh possivel alterar o valor com estadovi
+  const [counter, setCounter] = useState(0);
+  //Array [value, changeValueFunction]
+
+  function increment(){
+    setCounter(counter + 1);
+  }
   return(
     //JSX Javascript XML
     //<h1 >Hello World!</h1>
-    <Header>
-      Client Rest
-    </Header>
+    <div> 
+      <Header>
+        Counter: {counter}
+      </Header>
+      <button onClick={increment}>Add</button>
+    </div>
   );
 
 }
